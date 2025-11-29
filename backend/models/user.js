d76786e897ffa41f5580da4f03b8a9a23f69e158
s0nnarr <s0narr@protonmail.com>
2025-11-30 01:16:45 +0200

    Added updated User model with correct migration

---
+++ backend/models/user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import Model from 'sequelize';
+
+
+export default (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: { isEmail: true }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      role: {
+        type: DataTypes.ENUM("MP", "TST"),
+        allowNull: false,
+        defaultValue: "TST"
+      }
+
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
