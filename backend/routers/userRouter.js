702a4f77ca1e96e8f4b3a1f4c1204f69933d5561
Ionela113 <popaionela023@gmail.com>
2025-12-19 00:59:57 +0200

    User controllers si routes

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,10 +1,17 @@
 /* */
 
 import express from 'express';
-import { createUser } from '../controllers/user.js';
+import { createUser, getUsers, getUserById, updateUser,deleteUser } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post('/', createUser);
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+
+
 
 export default router;
