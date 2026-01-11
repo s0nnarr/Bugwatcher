78eb5fb78079fa938001a71537d234e073967b1d
Ionela113 <popaionela023@gmail.com>
2026-01-12 01:26:16 +0200

    Added bug controllers

--- backend/controllers/bugController.js
+++ backend/controllers/bugController.js
@@ -42,6 +42,87 @@ export const createBug = async (req, res) => {
     }
 }
 
+export const getBugsByProjectId = async (req, res) => {
+    try {
+        const { projectId } = req.params; 
+
+        const project = await Project.findByPk(projectId);
+        if (!project) {
+            return res.status(404).json({ message: "Project not found." });
+        }
+
+        const bugs = await Bug.findAll({
+            where: { projectId: projectId },
+            include: [
+                {
+                    model: User,
+                    as: 'assignedUser', 
+                    attributes: ['id', 'email', 'role'] 
+                }
+            ]
+        });
+
+        return res.status(200).json(bugs);
+    } catch (err) {
+        return res.status(500).json({
+            message: "Internal server error.",
+            error: err.message
+        });
+    }
+};
+
+export const updateBug = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description, severity, priority, status, assignedUserId } = req.body;
+
+        const bug = await Bug.findByPk(id);
+        if (!bug) {
+            return res.status(404).json({ message: "Bug not found." });
+        }
+
+        if (assignedUserId) {
+            const userExists = await User.findByPk(assignedUserId);
+            if (!userExists) {
+                return res.status(404).json({ message: "New assigned user not found." });
+            }
+        }
+
+        await bug.update({
+            title: title || bug.title,
+            description: description || bug.description,
+            severity: severity || bug.severity,
+            priority: priority || bug.priority,
+            status: status || bug.status,
+            assignedUserId: assignedUserId || bug.assignedUserId
+        });
+
+        return res.status(200).json({ message: "Bug updated successfully", bug });
+    } catch (err) {
+        return res.status(500).json({ message: "Internal server error.", error: err.message });
+    }
+};
+
+
+export const deleteBug = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const bug = await Bug.findByPk(id);
+        if (!bug) {
+            return res.status(404).json({ message: "Bug not found." });
+        }
+
+        await bug.destroy();
+        return res.status(200).json({ message: "Bug deleted successfully." });
+    } catch (err) {
+        return res.status(500).json({ message: "Internal server error.", error: err.message });
+    }
+};
+
+
+
+
 // TODO :
 /* CONTROLERE:
 - getBugsByProjectId
--- backend/routers/bugRouter.js
+++ backend/routers/bugRouter.js
@@ -3,9 +3,11 @@ import express from 'express';
 const router = express.Router();
 
 import {
-    createBug
+    createBug,getBugsByProjectId, updateBug, deleteBug
 } from '../controllers/bugController.js';
 
 router.post('/', createBug);
-
+router.get('/project/:projectId', getBugsByProjectId);
+router.put('/:id', updateBug);
+router.delete('/:id', deleteBug);
 export default router;
